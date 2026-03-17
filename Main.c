758913bc5fd8c15dd8a27d1eb3a77a3700ed636f
Mateus Fernandes <fernandesmateus0@outlook.com>
2026-03-17 01:29:09 +0000

    abrindo arquivo

--- Main.c
+++ Main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main
 {
+    FILE *file;
 
+    file = fopen("tabuleiroCheio.txt", r);
+
+    if (file == NULL){
+        printf("Erro ao abrir arquivo");
+        exit(1);
+    }
+
+    printf(file);
+
+    fclose(file);
     return 0;
 }
